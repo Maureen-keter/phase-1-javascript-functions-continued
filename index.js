6ea062ae328c103f4795fef1a01aab1d6cce596c
maureen <maureen.chelangat@student.moringaschool.com>
2023-09-21 17:58:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(act="roller-skate"){
+  return `This Saturday, I want to ${act}!`;
+}
+console.log(saturdayFun("bathe my dog"))
+
+const mondayWork=function(act="go to the office"){
+  return (`This Monday, I will ${act}.`);
+};
+mondayWork();
+mondayWork("work from home")
+
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+
+
+
+
